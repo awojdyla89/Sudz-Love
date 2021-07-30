0994fbe3a6eab0c6032f6f59e553d4c7c2b99fea
Adam Wojdyla <awojdyla@gmail.com>
2021-07-30 11:01:43 -0600

    boilerplate setup

---
+++ controllers/users.js
@@ -0,0 +1,74 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
+const { v4: uuidv4 } = require('uuid');
+const S3 = require('aws-sdk/clients/s3');
+const s3 = new S3(); // initialize the construcotr
+// now s3 can crud on our s3 buckets
+
+module.exports = {
+  signup,
+  login
+};
+
+function signup(req, res) {
+  console.log(req.body, req.file)
+
+  //////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////
+
+  // FilePath unique name to be saved to our butckt
+  const filePath = `${uuidv4()}/${req.file.originalname}`
+  const params = {Bucket: process.env.BUCKET_NAME, Key: filePath, Body: req.file.buffer};
+  //your bucket name goes where collectorcat is 
+  //////////////////////////////////////////////////////////////////////////////////
+  s3.upload(params, async function(err, data){
+    console.log(data, 'from aws') // data.Location is our photoUrl that exists on aws
+    const user = new User({...req.body, photoUrl: data.Location});
+    try {
+      await user.save();
+      const token = createJWT(user); // user is the payload so this is the object in our jwt
+      res.json({ token });
+    } catch (err) {
+      // Probably a duplicate email
+      res.status(400).json(err);
+    }
+
+
+
+  })
+  //////////////////////////////////////////////////////////////////////////////////
+ 
+}
+
+async function login(req, res) {
+  try {
+    const user = await User.findOne({email: req.body.email});
+    console.log(user, ' this user in login')
+    if (!user) return res.status(401).json({err: 'bad credentials'});
+    // had to update the password from req.body.pw, to req.body password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+        
+      if (isMatch) {
+        const token = createJWT(user);
+        res.json({token});
+      } else {
+        return res.status(401).json({err: 'bad credentials'});
+      }
+    });
+  } catch (err) {
+    return res.status(401).json(err);
+  }
+}
+
+
+/*----- Helper Functions -----*/
+
+function createJWT(user) {
+  return jwt.sign(
+    {user}, // data payload
+    SECRET,
+    {expiresIn: '24h'}
+  );
+}
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './LoginPage.css';
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage'
+import userService from '../../utils/userService';
+
+
+export default function LoginPage(props){
+    
+
+    return (
+      
+          
+             <h1>setup signup PAGE</h1>
+    
+      
+      );
+}
+
