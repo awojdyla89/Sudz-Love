90b6496b913c11fb95aaef4b713c85d5379c1b4a
Adam Wojdyla <awojdyla@gmail.com>
2021-08-02 09:07:05 -0600

    creates login component and functionality

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,15 +2,94 @@ import React, { useState } from 'react';
 import './LoginPage.css';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage'
 import userService from '../../utils/userService';
-
+import { useHistory , Link} from 'react-router-dom';
+import {
+    Button,
+    Form,
+    Grid,
+    Header,
+    Image,
+    Message,
+    Segment,
+  } from "semantic-ui-react";
 
 export default function LoginPage(props){
+    const [error, setError] = useState("");
+    const [state, setState] = useState({
+      email: "",
+      password: "",
+    });
+
+    const history = useHistory();
+
+    function handleChange(e){
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        try{
+            await userService.login(state);
+            props.handleSignUpOrLogin();
+            history.push("/");
+        } catch (err) {
+            setError(err.message);
+        }
+    }
     
 
     return (
-      
-          
-             <h1>setup Login PAGE</h1>
+        <>
+        <Grid
+          textAlign="center"
+          style={{ height: "100vh" }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h2" color="teal" textAlign="center">
+              <Image src="beerlogo.png" /> Log-in to your
+              account
+            </Header>
+            <Form autoComplete="off" onSubmit={handleSubmit}>
+              <Segment stacked>
+                <Form.Input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  value={state.email}
+                  onChange={handleChange}
+                  required
+                />
+                <Form.Input
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  value={state.password}
+                  onChange={handleChange}
+                  required
+                />
+                <Button
+                  color="teal"
+                  fluid
+                  size="large"
+                  type="submit"
+                  className="btn"
+                >
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
+              New to beer? <Link to="/signup">Sign Up</Link>
+            </Message>
+            {error ? <ErrorMessage error={error} /> : null}
+          </Grid.Column>
+        </Grid>
+      </>
     
       
       );
